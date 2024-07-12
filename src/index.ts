2a17df5ae933f63debc553f7f473fb1e2731d219
Ale-BeHe <alejandrobehe@outlook.com>
2024-07-12 19:32:11 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 3000
+
+
+app.get('/',(_req,res)=>{
+    res.send('Hellos, fellas!')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running at port ${PORT}`)
+})
