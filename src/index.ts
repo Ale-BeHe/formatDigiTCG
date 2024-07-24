a5b851c7b9581f638c36696fd079b68259583b09
Ale-BeHe <alejandrobehe@outlook.com>
2024-07-24 16:40:25 -0400

    helmet

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express' // ESModule
 import cors from 'cors'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+import helmet from 'helmet'
 import cards from './routes/cards'
 import user from './routes/users'
 
 const app = express()
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.log'), { flags: 'a'})
+
+app.use(helmet()) 
 app.use(cors())
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-lenght] - :response-time ms :date[web]', { stream: accessLogStream }))
 
 const PORT = 3000
 
